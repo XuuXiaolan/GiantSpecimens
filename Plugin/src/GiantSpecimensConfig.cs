80b56473ec7d48b466361a4e61497122b729e004
XuuXiao <ammaroaladel@Gmail.com>
2024-03-16 11:25:01 +0000

    config betterment

--- Plugin/src/GiantSpecimensConfig.cs
+++ Plugin/src/GiantSpecimensConfig.cs
@@ -22,7 +22,7 @@ namespace GiantSpecimens {
             configSpawnRateEntries = configFile.Bind("Moon Spawnrates", 
                                                 "RedWood Giant Spawn Weight.",
                                                 "ExperimentationLevel@50,AssuranceLevel@100,VowLevel@200,OffenseLevel@100,MarchLevel@200,RendLevel@200,DineLevel@100,TitanLevel@200,Modded@100,InfernisLevel@100,PorcerinLevel@200,EternLevel@150,Asteroid13Level@200,GratarLevel@100,PolarusLevel@150,AtlanticaLevel@25,CosmocosLevel@200,JunicLevel@150,GloomLevel@200,DesolationLevel@150,OldredLevel@100",
-                                                "Spawn Weight of the RedWood Giant in all vanilla moons + a universal modded option (doesn't work for LLL moons yet), just replace the number below with a custom spawnrate if you're changing it, do not change the format");
+                                                "Spawn Weight of the RedWood Giant in all vanilla moons + Wesley's moons modded option (Adding Generic's moons next, also doesn't work for LLL moons yet), just replace the number below with a custom spawnrate if you're changing it, do not change the format");
 
             ClearUnusedEntries(configFile);
             Plugin.Logger.LogInfo("Setting up config for Giant Specimen plugin...");
