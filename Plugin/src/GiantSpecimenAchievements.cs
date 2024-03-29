aaccdbad5ab432e4617b825d8f021cecf2a2b5b8
XuuXiao <ammaroaladel@Gmail.com>
2024-03-29 21:26:52 +0000

    adding achievement file for potential future achievements

---
+++ Plugin/src/GiantSpecimenAchievements.cs
@@ -0,0 +1,34 @@
+namespace GiantSpecimens {
+    /*public class CrushedByRedWoodGiant : BaseAchievement
+    {
+        [ModData(SaveWhen.OnSave, LoadWhen.OnLoad, SaveLocation.CurrentSave)]
+        private int JumpCount { get; set; }
+        
+        /// <inheritdoc />
+        public override string Name { get; set; } = "Red Pancake";
+        
+        /// <inheritdoc />
+        public override string? DisplayText { get; set; } = "You got crushed by the Redwood Giant's feet! (sicko)";
+        
+        /// <inheritdoc />
+        public override void Initialize()
+        {
+            PlayerJumpEvent += OnPlayerJump;
+        }
+
+        /// <inheritdoc />
+        public override void Uninitialize()
+        {
+            PlayerJumpEvent -= OnPlayerJump;
+        }
+        
+        private void OnPlayerJump()
+        {
+            JumpCount++;
+            if (JumpCount >= 10)
+            {
+                Complete();
+            }
+        }
+    }*/
+}
