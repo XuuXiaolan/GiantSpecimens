deacc795bf409dee676eb96180e2057632d6bcca
XuuXiao <ammaroaladel@Gmail.com>
2024-04-22 11:14:21 +0100

    lightning change script which is amazing

--- Plugin/Thunderstore/README.md
+++ Plugin/Thunderstore/README.md
@@ -37,6 +37,8 @@ The Giant Specimens, created by [@Xu Xiaolan](https://thunderstore.io/c/lethal-c
 
 -Redwood Plushie Icon sprite by [@mari0no1](https://thunderstore.io/c/lethal-company/p/Mari0no1/DoorsEscapeMusic/)
 
+-LGU compatibility and other scrap spawning on monster death help by [@SnackS](https://thunderstore.io/c/lethal-company/p/malco/Lategame_Upgrades/)
+
 -Uh, support me if you'd like on the support button next to Manual Download on thunderstore :p
 
 
--- Plugin/src/StormyEffect.cs
+++ Plugin/src/StormyEffect.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using DigitalRuby.ThunderAndLightning;
 using HarmonyLib;
 using UnityEngine;
@@ -11,13 +13,14 @@ public class StormyWeatherScript {
         Plugin.Logger.LogInfo(text);
         #endif
     }
+
     public static void SpawnLightningBolt(Vector3 strikePosition)
     {
         System.Random random = new System.Random(StartOfRound.Instance.randomMapSeed + 85);
-        Vector3 vector = strikePosition + Vector3.up * 160f + new Vector3((float)random.Next(-32, 32), 0f, (float)random.Next(-32, 32));
+        Vector3 offset = new Vector3((float)random.Next(-32, 32), 0f, (float)random.Next(-32, 32));
+        Vector3 vector = strikePosition + Vector3.up * 160f + offset;
 
         StormyWeather stormy = UnityEngine.Object.FindObjectOfType<StormyWeather>(true);
-
         if (stormy == null)
         {
             LogIfDebugBuild("StormyWeather not found");
@@ -35,24 +38,39 @@ public class StormyWeatherScript {
             return;
         }
 
-        // Change the color of the lightning bolt material to green
-        if (localLightningBoltPrefabScript.LightningMaterialMesh != null) {
-            Color green = new Color(1.0f, 0f, 0.0f, 1.0f);
-            localLightningBoltPrefabScript.LightningMaterialMesh.SetColor("_TintColor", green);
-        } else {
-            LogIfDebugBuild("LightningMaterial not found on prefab");
-        }
-
+        localLightningBoltPrefabScript.GlowWidthMultiplier = 2.5f;
+        localLightningBoltPrefabScript.DurationRange = new RangeOfFloats { Minimum = 0.6f, Maximum = 1.2f };
+        localLightningBoltPrefabScript.TrunkWidthRange = new RangeOfFloats { Minimum = 0.6f, Maximum = 1.2f };
         localLightningBoltPrefabScript.Camera = GameNetworkManager.Instance.localPlayerController.gameplayCamera;
-        localLightningBoltPrefabScript.AutomaticModeSeconds = 0.2f;
         localLightningBoltPrefabScript.Source.transform.position = vector;
         localLightningBoltPrefabScript.Destination.transform.position = strikePosition;
+        localLightningBoltPrefabScript.AutomaticModeSeconds = 0.2f;
+        localLightningBoltPrefabScript.Generations = 8;
+
+        MonoBehaviour host = localLightningBoltPrefabScript as MonoBehaviour;
+        host.StartCoroutine(ChangeLightningColor(localLightningBoltPrefabScript));
+
         localLightningBoltPrefabScript.CreateLightningBoltsNow();
 
         AudioSource audioSource = Object.Instantiate(stormy.targetedStrikeAudio);
         audioSource.transform.position = strikePosition + Vector3.up * 0.5f;
         audioSource.enabled = true;
-
+        audioSource.volume /= 4.0f;
         stormy.PlayThunderEffects(strikePosition, audioSource);
+    }
+
+    private static IEnumerator ChangeLightningColor(LightningBoltPrefabScript boltScript)
+    {
+        float duration = 1.0f; // Duration of the color change in seconds
+        float timer = 0f;
+        while (timer < duration)
+        {
+            float progress = timer / duration;
+            boltScript.GlowTintColor = Color.Lerp(new Color(0.8f, 0f, 0f, 0.25f), Color.blue, progress);
+            boltScript.LightningTintColor = Color.Lerp(new Color(0.7f, 0f, 0f, 0.25f), Color.cyan, progress);
+            boltScript.MainTrunkTintColor = Color.Lerp(new Color(0.6f, 0f, 0f, 0.25f), Color.white, progress);
+            timer += Time.deltaTime;
+            yield return null;
+        }
     }
 }
